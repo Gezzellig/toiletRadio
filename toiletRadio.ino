058b1b738dce91017c3a09d55cdb9cf5092b9b9b
Gezzellig <knaap@live.com>
2021-03-06 13:27:08 +0100

    working light sensor

--- toiletRadio.ino
+++ toiletRadio.ino
@@ -3,11 +3,14 @@
 #include "credentials.h"
 //const char* ssid = "wifiName" //Is retreived from credentials.h
 //const char* password = "wifiPassword" //Is retreived from credentials.h
-int ledPin = 2;
+#define LIGHT_PIN A0
+const int ledPin = 2;
+
 
 void setup() {
   // initialize digital pin LED_BUILTIN as an output.
   pinMode(ledPin, OUTPUT);
+  pinMode(LIGHT_PIN, INPUT);
   Serial.begin(9600);
   Serial.println("");
   Serial.println("");
@@ -26,8 +29,28 @@ void setup() {
 
 // the loop function runs over and over again forever
 void loop() {
-  digitalWrite(ledPin, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(1000);                       // wait for a second
-  digitalWrite(ledPin, LOW);    // turn the LED off by making the voltage LOW
-  delay(1000);                       // wait for a second
+  // reads the input on analog pin A0 (value between 0 and 1023)
+  int analogValue = analogRead(LIGHT_PIN);
+
+  Serial.print("Analog reading = ");
+  Serial.print(analogValue);   // the raw analog reading
+
+  // We'll have a few threshholds, qualitatively determined
+  if (analogValue < 10) {
+    Serial.println(" - Dark");
+  } else if (analogValue < 200) {
+    Serial.println(" - Dim");
+  } else if (analogValue < 500) {
+    Serial.println(" - Light");
+  } else if (analogValue < 800) {
+    Serial.println(" - Bright");
+  } else {
+    Serial.println(" - Very bright");
+  }
+
+  delay(500);
+//  digitalWrite(ledPin, HIGH);   // turn the LED on (HIGH is the voltage level)
+//  delay(1000);                       // wait for a second
+//  digitalWrite(ledPin, LOW);    // turn the LED off by making the voltage LOW
+//  delay(1000);                       // wait for a second
 }
