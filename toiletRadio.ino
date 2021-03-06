2397abe6703c1502874b3c2479752b9b8580f588
Gezzellig <knaap@live.com>
2021-03-06 15:22:40 +0100

    working radio, but with periodic noise

--- toiletRadio.ino
+++ toiletRadio.ino
@@ -1,11 +1,47 @@
 // the setup function runs once when you press reset or power the board
 #include <ESP8266WiFi.h> 
 #include "credentials.h"
+#include "AudioFileSourceICYStream.h"
+#include "AudioFileSourceBuffer.h"
+#include "AudioGeneratorMP3.h"
+#include "AudioOutputI2SNoDAC.h"
 //const char* ssid = "wifiName" //Is retreived from credentials.h
 //const char* password = "wifiPassword" //Is retreived from credentials.h
 #define LIGHT_PIN A0
 const int ledPin = 2;
 
+// To run, set your ESP8266 build to 160MHz, update the SSID info, and upload.
+const char *URL="http://jazz.streamr.ru/jazz-64.mp3";
+
+AudioGeneratorMP3 *mp3;
+AudioFileSourceICYStream *file;
+AudioFileSourceBuffer *buff;
+AudioOutputI2SNoDAC *out;
+
+// Called when a metadata event occurs (i.e. an ID3 tag, an ICY block, etc.
+void MDCallback(void *cbData, const char *type, bool isUnicode, const char *string) {
+  const char *ptr = reinterpret_cast<const char *>(cbData);
+  (void) isUnicode; // Punt this ball for now
+  // Note that the type and string may be in PROGMEM, so copy them to RAM for printf
+  char s1[32], s2[64];
+  strncpy_P(s1, type, sizeof(s1));
+  s1[sizeof(s1)-1]=0;
+  strncpy_P(s2, string, sizeof(s2));
+  s2[sizeof(s2)-1]=0;
+  Serial.printf("METADATA(%s) '%s' = '%s'\n", ptr, s1, s2);
+  Serial.flush();
+}
+
+// Called when there's a warning or error (like a buffer underflow or decode hiccup)
+void StatusCallback(void *cbData, int code, const char *string) {
+  const char *ptr = reinterpret_cast<const char *>(cbData);
+  // Note that the string may be in PROGMEM, so copy it to RAM for printf
+  char s1[64];
+  strncpy_P(s1, string, sizeof(s1));
+  s1[sizeof(s1)-1]=0;
+  Serial.printf("STATUS(%s) '%d' = '%s'\n", ptr, code, s1);
+  Serial.flush();
+}
 
 void setup() {
   // initialize digital pin LED_BUILTIN as an output.
@@ -25,32 +61,29 @@ void setup() {
   Serial.println("WiFi connected"); 
   Serial.print("ipAddress: ");
   Serial.println(WiFi.localIP());
+  audioLogger = &Serial;
+  file = new AudioFileSourceICYStream(URL);
+  file->RegisterMetadataCB(MDCallback, (void*)"ICY");
+  buff = new AudioFileSourceBuffer(file, 8192);
+  buff->RegisterStatusCB(StatusCallback, (void*)"buffer");
+  out = new AudioOutputI2SNoDAC();
+  mp3 = new AudioGeneratorMP3();
+  mp3->RegisterStatusCB(StatusCallback, (void*)"mp3");
+  mp3->begin(buff, out);
 }
 
 // the loop function runs over and over again forever
 void loop() {
-  // reads the input on analog pin A0 (value between 0 and 1023)
-  int analogValue = analogRead(LIGHT_PIN);
-
-  Serial.print("Analog reading = ");
-  Serial.print(analogValue);   // the raw analog reading
-
-  // We'll have a few threshholds, qualitatively determined
-  if (analogValue < 10) {
-    Serial.println(" - Dark");
-  } else if (analogValue < 200) {
-    Serial.println(" - Dim");
-  } else if (analogValue < 500) {
-    Serial.println(" - Light");
-  } else if (analogValue < 800) {
-    Serial.println(" - Bright");
+  static int lastms = 0;
+  if (mp3->isRunning()) {
+    if (millis()-lastms > 1000) {
+      lastms = millis();
+      Serial.printf("Running for %d ms...\n", lastms);
+      Serial.flush();
+    }
+    if (!mp3->loop()) mp3->stop();
   } else {
-    Serial.println(" - Very bright");
+    Serial.printf("MP3 done\n");
+    delay(1000);
   }
-
-  delay(500);
-//  digitalWrite(ledPin, HIGH);   // turn the LED on (HIGH is the voltage level)
-//  delay(1000);                       // wait for a second
-//  digitalWrite(ledPin, LOW);    // turn the LED off by making the voltage LOW
-//  delay(1000);                       // wait for a second
 }
